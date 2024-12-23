8f9d22752a8f44a04df11e673c1d415543ed3140
tetteh victor <victortetteh520@gmail.com>
2024-12-23 01:29:02 +0100

    done

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,57 +1,60 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
-
-/** Custom Hook */
 import { useFetchQestion } from '../hooks/FetchQuestions'
 import { updateResult } from '../hooks/setResult'
 
-
 export default function Questions({ onChecked }) {
+  const [checked, setChecked] = useState(undefined)
+  const { trace } = useSelector(state => state.questions);
+  const result = useSelector(state => state.result.result);
+  const [{ isLoading, apiData, serverError }] = useFetchQestion() 
 
-    const [checked, setChecked] = useState(undefined)
-    const { trace } = useSelector(state => state.questions);
-    const result = useSelector(state => state.result.result);
-    const [{ isLoading, apiData, serverError}] = useFetchQestion() 
+  const questions = useSelector(state => state.questions.queue[state.questions.trace])
+  const dispatch = useDispatch()
 
-    const questions = useSelector(state => state.questions.queue[state.questions.trace])
-    const dispatch = useDispatch()
-  
-    useEffect(() => {
-        dispatch(updateResult({ trace, checked}))
-    }, [checked])
-    
-    function onSelect(i){
-        onChecked(i)
-        setChecked(i)
-        dispatch(updateResult({ trace, checked}))
+  useEffect(() => {
+    if (checked !== undefined) {
+      dispatch(updateResult({ trace, checked }))
     }
+  }, [checked, trace])
 
+  function onSelect(i) {
+    onChecked(i)
+    setChecked(i)
+    dispatch(updateResult({ trace, checked: i }))
+  }
 
-    if(isLoading) return <h3 className='text-light'>isLoading</h3>
-    if(serverError) return <h3 className='text-light'>{serverError || "Unknown Error"}</h3>
+  if (isLoading) return <h3 className='text-light'>Loading...</h3>
+  if (serverError) {
+    return (
+      <div className='text-light'>
+        {serverError.message ? `Error: ${serverError.message}` : "Unknown Error"}
+      </div>
+    );
+  }
 
   return (
     <div className='questions'>
-        <h2 className='text-light'>{questions?.question}</h2>
+      <h2 className='text-light'>{questions?.question}</h2>
 
-        <ul key={questions?.id}>
-            {
-                questions?.options.map((q, i) => (
-                    <li key={i}>
-                        <input 
-                            type="radio"
-                            value={false}
-                            name="options"
-                            id={`q${i}-option`}
-                            onChange={() => onSelect(i)}
-                        />
-                             <label className='text-primary' htmlFor={`q${i}-option`}>{q}</label>
-                             <div className={`check ${result[trace] == i ? 'checked' : ''}`}></div>
-                    </li>
-                ))
-            }
-        </ul>
+      <ul key={questions?.id}>
+        {
+          questions?.options.map((q, i) => (
+            <li key={i}>
+              <input 
+                type="radio"
+                value={false}
+                name="options"
+                id={`q${i}-option`}
+                onChange={() => onSelect(i)}
+              />
+              <label className='text-primary' htmlFor={`q${i}-option`}>{q}</label>
+              <div className={`check ${result[trace] === i ? 'checked' : ''}`}></div>
+            </li>
+          ))
+        }
+      </ul>
     </div>
   )
 }
